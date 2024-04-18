45b09b24485ff715e82637e592cd5d69fabffeda
gonzagaa <iigust4vo@gmail.com>
2024-04-18 10:52:52 -0300

    update

--- script.js
+++ script.js
@@ -30,18 +30,8 @@ function closeMenu() {
     document.body.classList.remove('menu-expanded')
 }
 
-ScrollReveal().reveal('#home h1', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out'});
-
-ScrollReveal().reveal('#services .header', { distance: '200px', origin: 'left', duration: 500, easing: 'ease-in-out'});
-
-ScrollReveal().reveal('#services .first', { distance: '200px', origin: 'right', duration: 500, easing: 'ease-in-out'});
-
-ScrollReveal().reveal('#services .second', { distance: '200px', origin: 'right', duration: 500, delay: 300 ,easing: 'ease-in-out'});
-
-ScrollReveal().reveal(
-    '#values .vision, #values .mission, #values .value h2, #localizations header, #localizations .local',
-{ distance: '200px', origin: 'bottom', duration: 500 ,easing: 'ease-in-out'});
-
-ScrollReveal().reveal('#values .card', { distance: '200px', scale: 0.5, opacity: 0.5 ,duration: 700, easing: 'ease-in-out'});
-
-ScrollReveal().reveal('#areas details', { distance: '400px', origin: 'top', duration: 500, easing: 'ease-in-out'});
+AOS.init(
+    {
+        duration: 1200,
+    }
+);
